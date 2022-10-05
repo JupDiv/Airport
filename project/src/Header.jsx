e99bc4cfcf4878fffe3f66f13332f5cf9f156d26
JupDev <97832488+JupDiv@users.noreply.github.com>
2022-10-05 12:46:50 +0300

    fix

--- project/src/Header.jsx
+++ project/src/Header.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 
 const Header = () => {
+  const headerList = [
+    { id: 1, name: 'Пасажирам' },
+    { id: 2, name: 'Послуги IEV' },
+    { id: 3, name: 'VIP' },
+    { id: 4, name: 'Партнерам' },
+    { id: 5, name: 'Пресцентр' },
+  ];
   return (
     <header>
       <ul>
-        <li>Пасажирам</li>
-        <li>Послуги IEV</li>
-        <li>VIP</li>
-        <li>Партнерам</li>
-        <li>Пресцентр</li>
+        {headerList.map(({ id, name }) => (
+          <li key={id}>{name}</li>
+        ))}
       </ul>
     </header>
   );
